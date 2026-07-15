c7f034c4354d5bcfa6974fee5a05a16d91a4cc2f
Jorge Gomez <jgomezmail-dev@proton.me>
2026-07-15 19:16:07 -0400

    switching to regular docker

--- modules/dev/docker.nix
+++ modules/dev/docker.nix
@@ -9,10 +9,6 @@
   flake.modules.nixos.dev = { pkgs, ... }: {
     virtualisation.docker = {
       enable = true;
-      rootless = {
-        enable = true;
-        setSocketVariable = true;
-      };
     };
     users.users.${config.flake.aspects.owner.username}.extraGroups = [ "docker" ];
     environment.systemPackages = with pkgs; [
