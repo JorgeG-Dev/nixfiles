4b7b1eaf7031b02d2c0d20ed56d34dae1058aec9
Jorge Gomez <jgomezmail-dev@proton.me>
2026-07-18 12:29:27 -0400

    adding disko config and preservation confi

--- modules/hosts/little-ghost/disks.nix
+++ modules/hosts/little-ghost/disks.nix
@@ -1,40 +1,104 @@
 { inputs, config, ... }:
 {
   flake.diskoConfigurations.little-ghost = {
-    disko.devices = {
-      disk = {
-        main = {
-          device = "/dev/disk/by-id/nvme-WD_BLACK_SN850X_4000GB_25033U801640";
-          type = "disk";
-          content = {
-            type = "gpt";
-            partitions = {
-              ESP = {
-                size = "1G";
-                type = "EF00";
-                content = {
-                  type = "filesystem";
-                  format = "vfat";
-                  mountpoint = "/boot/";
-                  mountOptions = [ "umask=0077" ];
-                };
-              };
-              root = {
-                size = "100%";
-                content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/";
-                };
-              };
+    fileSystems."/nix".neededForBoot = true;
+    fileSystems."/persistent".neededForBoot = true;
+    disko.devices.nodev = {
+      "/" = {
+        fsType = "tmpfs";
+        mountOptions = [
+          "size=25%"
+          "mode=755"
+        ];
+      };
+    };
+    disko.devices.disk.main = {
+      device = "/dev/disk/by-id/nvme-WD_BLACK_SN850X_4000GB_25033U801640";
+      type = "disk";
+      content.type = "gpt";
+      content.partitions.ESP = {
+        size = "1G";
+        type = "EF00";
+        content = {
+          type = "filesystem";
+          format = "vfat";
+          mountpoint = "/boot";
+          mountOptions = [ "umask=0077" ];
+        };
+      };
+      content.partitions.swap = {
+        size = "64G";
+        content = {
+          type = "swap";
+          resumeDevice = true;
+        };
+      };
+      content.partitions.root = {
+        name = "root";
+        size = "100%";
+        content = {
+          type = "btrfs";
+          extraArgs = [ "-f" ];
+
+          subvolumes = {
+            "/persistent" = {
+              mountOptions = [
+                "subvol=persistent"
+                "compress=zstd"
+                "noatime"
+              ];
+              mountpoint = "/persistent";
+            };
+
+            "/nix" = {
+              mountOptions = [
+                "subvol=nix"
+                "compress=zstd"
+                "noatime"
+              ];
+              mountpoint = "/nix";
             };
           };
         };
-        nodev = {
-          "/tmp" = {
-            fsType = "tmpfs";
-            mountOptions = [ "size=200M" ];
-          };
+      };
+    };
+  };
+
+  flake.modules.nixos.little-ghost = {
+    preservation = {
+      enable = true;
+      preserveAt."/persistent" = {
+        files = [
+          {
+            file = "/etc/machine-id";
+            inInitrd = true;
+          }
+        ];
+
+        directories = [
+          "/var/lib/systemd/timers"
+          {
+            directory = "/var/lib/nixos";
+            inInitrd = true;
+          }
+          "/var/lib/docker"
+          "/var/log"
+          "/etc/NetworkManager/system-connections"
+          "/tmp"
+        ];
+
+        users.${config.flake.aspects.owner.username} = {
+          files = [ ];
+          directories = [
+            "projects"
+            "Documents"
+            "Pictures"
+            "Downloads"
+            ".local/state/wireplumber"
+            ".local/state/nvim"
+            ".ssh"
+            ".mozilla"
+          ];
         };
       };
     };
