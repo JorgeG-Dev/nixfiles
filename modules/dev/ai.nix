7c94b8b41d2650a320231f574df0d367b6729df8
Jorge Gomez <jgomezmail-dev@proton.me>
2026-08-23 18:28:37 -0400

    adding claude code

---
+++ modules/dev/ai.nix
@@ -0,0 +1,25 @@
+{
+  self,
+  inputs,
+  config,
+  pkgs,
+  wlib,
+  ...
+}:
+{
+  flake.modules.nixos.dev =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [
+        claude-code
+      ];
+    };
+
+  flake.modules.darwin.dev =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [
+        claude-code
+      ];
+    };
+}
